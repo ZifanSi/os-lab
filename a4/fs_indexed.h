df9f0904c2b38ad173fa7b357d7d25e5478623ee
ZifanSi <siz@mcmaster.ca>
2026-04-06 00:10:01 +0000

    Update

--- a4/fs_indexed.h
+++ a4/fs_indexed.h
@@ -3,17 +3,20 @@
 
 #include <stddef.h>
 
+// FS configuration
 #define FS_TOTAL_BLOCKS 64
 #define FS_BLOCK_SIZE 1024
 #define FS_MAX_FILES 16
 #define FS_MAX_FILENAME 64
 #define FS_MAX_DATA_BLOCKS_PER_FILE 256
 
+// Free block list node
 typedef struct BlockNode {
     int blockNumber;
     struct BlockNode *next;
 } BlockNode;
 
+// File metadata and indexed block pointers
 typedef struct FileInformationBlock {
     int inUse;
     int fibId;
@@ -24,6 +27,7 @@ typedef struct FileInformationBlock {
     int dataBlocks[FS_MAX_DATA_BLOCKS_PER_FILE];
 } FileInformationBlock;
 
+// System state tracking
 typedef struct VolumeControlBlock {
     int totalBlocks;
     int blockSize;
@@ -36,6 +40,7 @@ typedef struct VolumeControlBlock {
     BlockNode *freeTail;
 } VolumeControlBlock;
 
+// Main system structure
 typedef struct FileSystem {
     VolumeControlBlock vcb;
     FileInformationBlock files[FS_MAX_FILES];
@@ -43,13 +48,16 @@ typedef struct FileSystem {
 
 extern FileSystem fs;
 
+// Lifecycle
 void initFS(void);
 void destroyFS(void);
 
+// Operations
 int createFile(const char *filename, size_t sizeBytes);
 int deleteFile(const char *filename);
 void listFiles(void);
 
+// Utility
 int allocateFreeBlock(void);
 void returnFreeBlock(int blockNumber);
 int getFileInformationBlockID(void);
